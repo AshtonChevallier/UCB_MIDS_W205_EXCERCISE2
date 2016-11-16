1e2f033f1e59b4991eaa66fc86f39021613c653e
AshtonChevallier <ashtonc@gmail.com>
2016-11-16 09:21:10 -0800

    Update wordcount.py

--- tweetwordcount/src/bolts/wordcount.py
+++ tweetwordcount/src/bolts/wordcount.py
@@ -19,11 +19,22 @@ class WordCounter(Bolt):
         # Database name: Tcount 
         # Table name: Tweetwordcount 
         # you need to create both the database and the table in advance.
-        
-
-        # Increment the local count
+          # Increment the local count
         self.counts[word] += 1
         self.emit([word, self.counts[word]])
+        
+        conn = psycopg2.connect(database="Tcount", user="postgres", password="pass", host="localhost", port="5432")
+        cur = conn.cursor()
+        if self.counts[word] == 1
+            cur.execute("INSERT INTO Tweetwordcount (word,count) VALUES (" + word + "," + self.counts[word] + ")")
+        else 
+            cur.execute("UPDATE Tweetwordcount SET count = " + self.counts[word] + " WHERE word = " + word ")" )
+        conn.commit()
+        cur.close()
+        conn.close()
+        
+
+      
 
         # Log the count - just to see the topology running
         self.log('%s: %d' % (word, self.counts[word]))
